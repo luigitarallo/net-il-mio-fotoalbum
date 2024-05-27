1a3ffa4325551445c039df8ebefef3542051de64
luigitarallo <tarallo.gigi@gmail.com>
2024-05-27 12:02:35 +0200

    Add PhotoContext, PhotoManager and PhotoController

---
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class PhotoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Data/PhotoManager.cs
@@ -0,0 +1,6 @@
+﻿namespace net_il_mio_fotoalbum.Data
+{
+    public class PhotoManager
+    {
+    }
+}
