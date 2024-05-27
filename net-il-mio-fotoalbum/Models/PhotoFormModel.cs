dee56003138a370e21115e83c17eeabbf9375ab5
luigitarallo <tarallo.gigi@gmail.com>
2024-05-27 12:56:52 +0200

    add CreateCategories to PhotoFormModel

--- net-il-mio-fotoalbum/Models/PhotoFormModel.cs
+++ net-il-mio-fotoalbum/Models/PhotoFormModel.cs
@@ -1,9 +1,39 @@
-﻿namespace net_il_mio_fotoalbum.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using net_il_mio_fotoalbum.Data;
+
+namespace net_il_mio_fotoalbum.Models
 {
     public class PhotoFormModel
     {
         public Photo Photo { get; set; }
+        public List<SelectListItem>? Categories { get; set; } // To show categories to select
+        public List<string> SelectedCategories { get; set; }
 
+        public PhotoFormModel() { }
 
+        public PhotoFormModel(Photo photo) 
+        {
+            this.Photo = photo;
+        }
+
+        public void CreateCategories()
+        {
+            this.Categories = new List<SelectListItem>();
+            if (this.SelectedCategories == null) 
+            {
+                this.SelectedCategories = new List<string>();
+            }
+            var categoriesFromDb = PhotoManager.GetAllCategories();
+            foreach (var category in categoriesFromDb)
+            {
+                bool isSelected = this.SelectedCategories.Contains(category.CategoryId.ToString());
+                this.Categories.Add(new SelectListItem()
+                {
+                    Text = category.Name,
+                    Value = category.CategoryId.ToString(),
+                    Selected = isSelected
+                });
+            }
+        }
     }
 }
