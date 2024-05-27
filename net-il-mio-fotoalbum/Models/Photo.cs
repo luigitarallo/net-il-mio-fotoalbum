d2a8731795dfe61220b0c6b626d4bd83ccb1638b
luigitarallo <tarallo.gigi@gmail.com>
2024-05-27 11:20:10 +0200

    Add Photo Model and entityFramework packages

---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        [Key] public int PhotoId { get; set; }
+        [Required] public string Title { get; set; }
+        [Required] public string Description { get; set; }
+        [Required] public byte[] PhotoFile { get; set; }
+        public bool IsVisible { get; set; }
+        public string PhotoSrc => $"data:image/jpeg;base64,{Convert.ToBase64String(PhotoFile)}";
+        public Photo() { }
+
+        public Photo(string title, string description, byte[] photoFile, bool isVisible) 
+        {
+            Title = title;
+            Description = description;
+            PhotoFile = photoFile;
+            IsVisible = isVisible;
+        }
+    }
+}
