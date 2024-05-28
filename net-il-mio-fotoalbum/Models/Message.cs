ab9c82cdc1be112f344d7c88653192dc8add8a44
luigitarallo <tarallo.gigi@gmail.com>
2024-05-28 17:39:51 +0200

    Add model for Message

---
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        
+        public Message() { }
+    }
+}
